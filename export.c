1aca8eb7a67f64f31f98de4b7214d1d6f60f49e5
E1-80732-PARAG <parag5265@gmail.com>
2024-01-01 15:50:12 +0530

    kfifo allocation

--- export.c
+++ export.c
@@ -4,14 +4,43 @@
 #include <linux/kfifo.h>
 #include <linux/moduleparam.h>
 
+int ret;
+//===========================================================
+// 1) a. Pass kfifo size as module parameter.
 int kfifo_size = 1;
 module_param(kfifo_size, int, 0644);
+
+//===========================================================
+
+struct kfifo my_kfifo;
+//===========================================================
+//===========================================================
+//===========================================================
+
 
 
 static __init int export_init(void)
 {
 	printk(KERN_INFO " %s : export_init() function start \n", THIS_MODULE->name);
 	printk(KERN_INFO " %s :   size = %d \n", THIS_MODULE->name, kfifo_size);
+	
+	// 2) In init, create a kernel fifo with given size, if size is less than 1024 bytes. If size is more than 1024 return error.
+	if(kfifo_size > 1024 )
+	{
+		printk(KERN_ERR " %s : kfifo size is greater than 1024 \n", THIS_MODULE->name);
+		return -1;
+	}
+	
+	// kfifo allocation
+	ret = kfifo_alloc(&my_kfifo, kfifo_size , GFP_KERNEL);
+	if(ret < 0)
+	{
+		printk(KERN_ERR " %s : kfifo alloc is failed\n", THIS_MODULE->name);
+		return -1;
+	}
+	
+	
+
 
 	return 0;
 }
